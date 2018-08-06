622cbf3301396bc39b684858206655d10a4d7893
lavakusa Bitra <lavakusa003@gmail.com>
2018-08-06 11:55:33 +0530

    Removed Project

--- EmployeeManagementSystem/EmployeeManagementSystem_MDM_Test/stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// EmployeeManagementSystem_MDM_Test.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
